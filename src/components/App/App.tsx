dfac894e54bfb9b369a7c11d92ee9cf483b71963
Pooya Tolideh <pooyatolideh@Pooyas-Laptop.local>
2024-08-20 12:05:35 -0400

    add progress bar logic and animation

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,70 @@
-import { ReactNode, RefObject, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+const TIME_OUT_DURATION = 2000;
 
 function App() {
-  const [dur, setDur] = useState(0);
-  const [status, setStatus] = useState<"start" | "end" | "idle">("idle");
+  const [status, setStatus] = useState<"keydown" | "completed" | "idle">(
+    "idle"
+  );
+  const [progressWidth, setProgressWidth] = useState(0);
+
   const containerRef = useRef<HTMLElement>(null);
 
+  // start listening to keydown if in idle state
   useEffect(() => {
-    if (dur === 1) {
-      setStatus("start");
-      setTimeout(() => {
-        setStatus("end");
-      }, 1000);
+    if (status === "idle") {
+      const handler = () => {
+        setStatus("keydown");
+      };
+      window.addEventListener("keydown", handler);
+      return () => {
+        window.removeEventListener("keydown", handler);
+      };
     }
-  }, [dur]);
+  }, [status]);
 
+  // track how long the key been pressed
+  // signal the end once completed
+  // update progress bar
   useEffect(() => {
-    console.log({ status });
-    if (status === "end") {
+    let intervalToken: NodeJS.Timeout | null = null;
+    let timeoutToken: NodeJS.Timeout | null = null;
+
+    const reset = () => {
+      if (intervalToken) clearInterval(intervalToken);
+      if (timeoutToken) clearTimeout(timeoutToken);
+      setProgressWidth(0);
+    };
+
+    if (status === "keydown") {
+      intervalToken = setInterval(() => {
+        setProgressWidth((prevWidth) => prevWidth + 1);
+      }, Math.ceil(TIME_OUT_DURATION / 100));
+
+      timeoutToken = setTimeout(() => {
+        setStatus("completed");
+      }, TIME_OUT_DURATION);
+    } else {
+      reset();
+    }
+
+    return reset;
+  }, [status]);
+
+  // reset status back to idle
+  useEffect(() => {
+    const handler = () => {
+      setStatus("idle");
+    };
+    window.addEventListener("keyup", handler);
+    return () => {
+      window.removeEventListener("keyup", handler);
+    };
+  }, []);
+
+  // change color when key's been pressed for full duration
+  useEffect(() => {
+    if (status === "completed") {
       const color = [255, 255, 255]
         .map((val) => `${Math.floor(Math.random() * val)}`)
         .join(",");
@@ -26,36 +74,22 @@ function App() {
     }
   }, [status]);
 
-  useEffect(() => {
-    if (status !== "end") {
-      const handler = () => {
-        setDur((prevDur) => prevDur + 1);
-      };
-
-      window.addEventListener("keydown", handler);
-      return () => {
-        window.removeEventListener("keydown", handler);
-      };
-    }
-  }, [setDur, status]);
-
-  useEffect(() => {
-    const handler = () => {
-      setDur(0);
-      setStatus("idle");
-    };
-    window.addEventListener("keyup", handler);
-    return () => {
-      window.removeEventListener("keyup", handler);
-    };
-  }, []);
-
   return (
-    <main ref={containerRef} className="container mx-auto">
+    <main ref={containerRef} className="container mx-auto bg-slate-200">
       <div className="flex flex-col items-center justify-center h-screen ">
         <h1 className="mx-4 font-bold mb-10 mt-2">Press and Hold "X" to Act</h1>
-        <div className="">
-          X - <span className="dur">{dur}</span>
+        <div
+          id="key-container"
+          className="flex items-center justify-center relative w-16 h-16 border-2 border-white text-4xl rounded-lg"
+        >
+          <span className="relative z-10">X</span>
+          <span
+            id="progress-ui"
+            style={{
+              width: `${progressWidth}%`,
+            }}
+            className="absolute h-full bg-white z-0"
+          ></span>
         </div>
       </div>
     </main>
