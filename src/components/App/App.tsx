6d44c362dffb3edabe7b3d503a88a4d0fbc8c94a
Pooya Tolideh <pooyatolideh@Pooyas-Laptop.local>
2024-08-19 22:26:14 -0400

    add logic to track and display keydown duration

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,30 @@
-import { ReactNode, useState } from "react";
+import { ReactNode, RefObject, useEffect, useRef, useState } from "react";
 
+function App() {
+  const durRef = useRef<HTMLSpanElement>(null);
+  const trackerRef = useRef(0);
 
+  useEffect(() => {
+    const handler = () => {
+      console.log("keydown");
+      trackerRef.current++;
+      if (durRef.current) durRef.current.innerText = String(trackerRef.current);
+    };
+    window.addEventListener("keydown", handler);
+    return () => {
+      window.removeEventListener("keydown", handler);
+    };
+  }, []);
 
 function App() {
 
   return (
     <main className="container mx-auto">
       <div className="flex flex-col items-center justify-center h-screen ">
-          <h1 className="mx-4 font-bold mb-10 mt-2">REACT TYPESCRIPT APP</h1>
+        <h1 className="mx-4 font-bold mb-10 mt-2">Press and Hold "X" to Act</h1>
+        <div className="">
+          X - <span className="dur" ref={durRef}></span>
+        </div>
       </div>
     </main>
   );
